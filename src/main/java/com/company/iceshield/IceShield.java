78f0cb414fffa84f7e6d67b13787770d263b4450
Luidev02 <Luidev02@DESKTOP-DE2HDQ2>
2023-11-03 11:02:55 -0500

    diseño de login

---
+++ src/main/java/com/company/iceshield/IceShield.java
@@ -0,0 +1,11 @@
+
+package com.company.iceshield;
+
+public class IceShield {
+
+    
+    public static void main(String[] args) {
+        jLogin login = new jLogin();
+        login.setVisible(true);
+    }
+}
